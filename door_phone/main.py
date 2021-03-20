1a9b8e04d8587fbd84811467c1279bae7277cfd6
averak <abe12@mccc.jp>
2021-03-20 17:45:19 +0900

    door_phone: init commit

---
+++ door_phone/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import argparse
+
+
+if __name__ == '__main__':
+    # options
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
+    args = parser.parse_args()
