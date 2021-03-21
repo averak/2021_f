9715d2cd6057c36e55e1d618438708d1ecf6ed86
averak <abe12@mccc.jp>
2021-03-21 11:36:24 +0900

    [feat] 5を認識

--- asr-server/core/config.py
+++ asr-server/core/config.py
@@ -43,7 +43,7 @@ METRICS: list = ['accuracy']
 
 # class config
 # CLASSES: tuple = ('1', '2', '3', '4', '5', '6', '7', '8', '9')
-CLASSES: tuple = ('1', '2', '3', '4')
+CLASSES: tuple = ('1', '2', '3', '4', '5')
 N_CLASSES: int = len(CLASSES)
 
 # api config
